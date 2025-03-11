6ddd1d5b507334b59321a4c70f04021b712ac4a8
nabeel rao <nabeelr531@gmail.com>
2025-03-11 10:57:00 -0700

    redux toolkit implmentation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,33 @@
+"use client"
+
 import { DM_Sans } from "next/font/google";
 import './assets/scss/tailwind.scss'
 import './assets/css/materialdesignicons.css'
+import { Provider } from 'react-redux';
+// import { store } from '../redux/store';
+import { store } from '../redux/store';
+
+
 
 const dm_sans = DM_Sans({ 
   subsets: ["latin"],
   variable: '--font-dm_sans',
 });
 
-export const metadata = {
-  title: "Cartzio - Next Js Fashion Store eCommerce Tailwind CSS Landing Template",
-  description: "Next Js Fashion Store eCommerce Tailwind CSS Landing Template",
-};
+// export const metadata = {
+//   title: "Cartzio - Next Js Fashion Store eCommerce Tailwind CSS Landing Template",
+//   description: "Next Js Fashion Store eCommerce Tailwind CSS Landing Template",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light scroll-smooth dm_sans" dir="ltr">
-      <body className={`${dm_sans.variable} dark:bg-slate-900`}>{children}</body>
+      <body className={`${dm_sans.variable} dark:bg-slate-900`}>
+        <Provider store={store} >
+        {children}
+        </Provider>
+        
+        </body>
     </html>
   );
 }
---
+++ src/redux/apiSlice.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  tagTypes: ['Post'],
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => '/posts',
+      providesTags: ['Post'],
+    }),
+    getPostById: builder.query({
+      query: (id) => `/posts/${id}`,
+    }),
+    createPost: builder.mutation({
+      query: (newPost) => ({
+        url: '/posts',
+        method: 'POST',
+        body: newPost,
+      }),
+      invalidatesTags: ['Post'],
+    }),
+    updatePost: builder.mutation({
+      query: ({ id, ...updatedPost }) => ({
+        url: `/posts/${id}`,
+        method: 'PUT',
+        body: updatedPost,
+      }),
+      invalidatesTags: ['Post'],
+    }),
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `/posts/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Post'],
+    }),
+  }),
+});
+
+export const {
+  useGetPostsQuery,
+  useGetPostByIdQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = apiSlice;
