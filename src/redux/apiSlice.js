c7df01a160ba181ea493d2544b4fb43a9bee1dbb
nabeel rao <nabeelr531@gmail.com>
2025-03-11 13:31:10 -0700

    authslice

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -1,13 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { authQuery } from './authSlice';
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  baseQuery:authQuery,
+  // baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
   tagTypes: ['Post'],
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: () => '/posts',
       providesTags: ['Post'],
+    
     }),
     getPostById: builder.query({
       query: (id) => `/posts/${id}`,
